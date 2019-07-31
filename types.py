d7cfd3407f95da85f4f96ac80b6a88f8aee2a589
saba1792 <51935108+saba1792@users.noreply.github.com>
2019-07-30 17:22:46 -0700

    Add files via upload

---
+++ types.py
@@ -0,0 +1,26 @@
+from decimal import *
+
+def decimal():
+    a = Decimal('.10')
+    b = Decimal('.30')
+    sum = (3 * a) + b #can be +,-,/,//
+    print(f'Decimal fn result is {sum}')
+    print(type(sum))
+    integer()
+
+def integer():
+    c = 7
+    d = 3
+    sum1 = c + d  #can be +,-,/,//
+    print(f'Integer fn result is {sum1}')
+    print(type(sum1))
+    floating()
+
+def floating():
+    e = .1
+    f = .3
+    sum2 = e + e - f  #can be any arithmetic operator
+    print(f'Floating fn result is {sum2}')
+    print(type(sum2))
+
+decimal()
